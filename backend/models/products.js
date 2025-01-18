a4f5072fb904fd8b3af67ae39ddb20ae61c4fb99
Ghifani <ghifani03@gmail.com>
2025-01-18 16:27:46 +0700

    🗃️ Create table
    Create table products

--- backend/models/products.js
+++ backend/models/products.js
@@ -1,2 +1,29 @@
 const { Model, DataTypes } = require('sequelize')
+const sequelize = require('../db.config')
 
+class Product extends Model { }
+
+Product.init({
+    nip: {
+        type: DataTypes.STRING,
+        unique: true
+    },
+    nama: {
+        type: DataTypes.STRING
+    },
+    jenisBarang: {
+        type: DataTypes.STRING
+    },
+    harga: {
+        type: DataTypes.INTEGER
+    },
+    image: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    sequelize,
+    modelName: 'Products'
+})
+
+module.exports = Product
